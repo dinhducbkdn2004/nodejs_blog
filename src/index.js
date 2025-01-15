665499ce8ddade42c29f84216660be63a8d87139
DinhDuc <dinhducbkdn2004@gmail.com>
2025-01-15 10:03:44 +0700

    Install prettier, lint-staged husky

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ const port = 3000;
 
 const route = require("./routes");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.static(
+    path.join(
+      __dirname,
+
+      "public"
+    )
+  )
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
